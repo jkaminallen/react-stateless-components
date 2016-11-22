2240e5c0be7e0f52d3ef6bcfabaa0ef741e8a281
jkaminallen <kaminallenj@gmail.com>
2016-11-22 12:18:24 -0500

    uploading

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// main.js
+import './app.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
